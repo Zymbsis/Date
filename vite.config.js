1fc407a0c5900767b8e7e029bdccdae3c80df7d1
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2024-01-22 20:49:00 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -3,28 +3,37 @@ import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
-export default defineConfig(({ command }) => {
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-    root: 'src',
-    build: {
-      sourcemap: true,
-
-      rollupOptions: {
-        input: glob.sync('./src/*.html'),
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
+export default defineConfig(
+  ({ command }) => {
+    return {
+      define: {
+        [command === 'serve' ? 'global' : '_global']: {},
       },
-      outDir: '../dist',
+      root: 'src',
+      build: {
+        sourcemap: true,
+
+        rollupOptions: {
+          input: glob.sync('./src/*.html'),
+          output: {
+            manualChunks(id) {
+              if (id.includes('node_modules')) {
+                return 'vendor';
+              }
+            },
+            entryFileNames: 'commonHelpers.js',
+          },
+        },
+        outDir: '../dist',
+      },
+      plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+    };
+  },
+  {
+    build: {
+      rollupOptions: {
+        external: ['izitoast/dist/css/izitoast.min.css'],
+      },
     },
-    plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
-  };
-});
+  }
+);
