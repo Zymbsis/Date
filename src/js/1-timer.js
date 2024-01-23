2e3c505b44a814eabb462b50edc255698cd6e061
Zymbsis <Aleksandra.aleksashka1809@gmail.com>
2024-01-23 22:49:53 +0100

    commit

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -5,8 +5,28 @@ import 'flatpickr/dist/flatpickr.min.css';
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 import firstIcon from '../img/bi_x-octagon.svg';
+import secondIcon from '../img/bi_x-lg.svg';
 
 const button = document.querySelector('[data-start]');
+function createPopUp() {
+  iziToast.show({
+    backgroundColor: '#EF4040',
+    messageColor: '#fff',
+    messageSize: 16,
+    message: 'Please choose a date in the future ',
+    position: 'topRight',
+    iconUrl: firstIcon,
+    close: false,
+    buttons: [
+      [
+        `<button type="button" style="background-color: #EF4040"><img src=${secondIcon}></button>`,
+        function (instance, toast) {
+          instance.hide({ transitionOut: 'fadeOut' }, toast);
+        },
+      ],
+    ],
+  });
+}
 let userSelectedDate;
 let currentDate;
 const options = {
@@ -15,33 +35,22 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    // userSelectedDate = selectedDates[0];
     console.log(selectedDates[0]);
-
     currentDate = new Date();
     if (selectedDates[0].getTime() <= currentDate.getTime()) {
-      iziToast.error({
-        backgroundColor: '#EF4040',
-        messageColor: '#fff',
-        messageSize: 16,
-        message: 'Please choose a date in the future ',
-        position: 'topRight',
-        iconUrl: firstIcon,
-        close: false,
-        timeout: 60000,
-        buttons: [
-          [
-            '<button type="button" style="background-color: #EF4040"><img src="../img/bi_x-lg.svg"></button>',
-          ],
-        ],
-      });
-
+      createPopUp();
       button.classList.remove('active-button');
     } else {
       userSelectedDate = selectedDates[0];
-      console.log('Correct date');
       button.classList.add('active-button');
+      button.addEventListener('click', some);
+      return userSelectedDate;
     }
   },
 };
 
 flatpickr('#datetime-picker', options);
+function some() {
+  console.log(userSelectedDate);
+}
